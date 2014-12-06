02f0c598e09368b13f262b6fd72bcf84820f0429
akhare121 <Ameya@587217472.guest.umich.net>
2014-12-05 22:25:42 -0500

    First Commit

---
+++ euler2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+const int LIMIT = 4000000;
+
+int main () {
+    int first = 1, second = 2;
+    int number = first + second, sum = 2;
+    
+    while (number < LIMIT) {
+        if (number%2 == 0)
+            sum+=number;
+        
+        first = second;
+        second = number;
+        number = first+second;
+    }
+    
+    cout << sum << endl;
+}
---
+++ euler3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const unsigned long long TARGET = 600851475143;
+
+int main () {
+    long fac = 1;
+    
+    
+}
