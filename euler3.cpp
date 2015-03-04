e662e16af9d6e4bbfe9949712e8a49ffff416fb9
akhare121 <akhare121@gmail.com>
2015-03-03 22:44:34 -0800

    blah

--- euler3.cpp
+++ euler3.cpp
@@ -6,7 +6,17 @@ using namespace std;
 const unsigned long long TARGET = 600851475143;
 
 int main () {
-    long fac = 1;
+    long fac = 0;
     
-    
+    for (int i = 1; i < TARGET; i++) {
+        if (isPrime(i)) fac = i;
+    }
+}
+
+bool isPrime (long num) {
+    for (int i = 2; i < num/2; i++) {
+        if (num%i == 0) return false
+    }
+            
+    return true;
 }
